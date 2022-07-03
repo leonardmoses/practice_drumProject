def239c9fc2eee86fc256611b6e956310afbe255
Leonard Moses <qalexeon@Polaris.lan>
2022-07-03 18:34:19 -0400

    added a switch function and play "w" button

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let btn = document.querySelectorAll('.drum')
-// let audioTom1 = new Audio('/sounds/tom-1.mp3')
+
 
 for (let i=0; i<btn.length; i++) {
 
     document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-        let btnNew = this
-        btnNew.style.color = "blue";
-        console.log(btnNew)
+
+        let buttonpressed = this.innerHTML
+
+        switch (buttonpressed) {
+            case 'w':
+                let audioTom1 = new Audio('/sounds/tom-1.mp3')
+                audioTom1.play();
+                break;
+            
+            
+            default:
+                break;
+        }
+
+        console.log(buttonpressed)
+
     })
 }
